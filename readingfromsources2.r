69d8d1fdef6ace05eea8cad51a8a67fe6e7ab5e4
jmfti <mira.javi@gmail.com>
2015-08-17 14:13:10 +0200

    reading foreign data

--- readingfromsources2.r
+++ readingfromsources2.r
@@ -56,3 +56,33 @@ hometl <- GET("https://api.twitter.com/1.1/statuses/home_timeline.json", sig)
 json <- content(hometl)
 json2 <- jsonlite::fromJSON(jsonlite::toJSON(json))
 json2[1,1:4]
+
+### reading foreign data
+# read.arff - reads weka files
+# read.dta - reads Stata files
+# read.mtp - reads minitab files
+# read.octave - reads octave files
+# read.spss - reads SPSS files
+# read.xport - reads SAS files
+
+## RPostgreSQL https://cran.r-project.org/web/packages/RPostgreSQL/index.html
+
+## reading images
+# jpeg https://cran.r-project.org/web/packages/jpeg/index.html
+# readbitmap https://cran.r-project.org/web/packages/readbitmap/index.html
+# png https://cran.r-project.org/web/packages/png/index.html
+# EBImage (bioconductor) http://bioconductor.org/packages/release/bioc/html/EBImage.html
+
+
+## GIS data (geographic data)
+# rgdal https://cran.r-project.org/web/packages/rgdal/index.html
+# rgeos https://cran.r-project.org/web/packages/rgeos/index.html
+# raster https://cran.r-project.org/web/packages/raster/index.html
+
+## reading music data
+# tuneR https://cran.r-project.org/web/packages/tuneR/index.html
+# seewave https://cran.r-project.org/web/packages/seewave/index.html
+
+
+## sqldf makes possible to send SQL queries to our data frames
+# used for working in other formats : install.packages("foreign") includes spss data files
